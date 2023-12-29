bd4842597a1d4ae5d7190fb82c6099735c987861
SeniorIntern <tnikhil.official@gmail.com>
2023-12-29 22:28:17 +0545

    Build the GenreList component

---
+++ app/GenreList.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { Flex } from '@radix-ui/themes'
+import useGenres from './hooks/useGenres'
+
+const GenreList = () => {
+  const { genres, isLoading, error } = useGenres()
+  console.log('genres===', genres)
+
+  return (
+    <div className='ml-4'>
+      {isLoading && <p>Loading...</p>}
+      {error && <p className="text-red-600 text-center">{error}</p>}
+      <Flex direction="column" className='gap-4'>
+        {genres.map(genre => (
+          <div key={genre._id}>
+            <p className='text-[1.15rem]'>{genre.name}</p>
+          </div>
+        ))}
+      </Flex>
+    </div>
+  )
+}
+
+export default GenreList
