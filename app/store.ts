a665d057b0578185c0a9e4a7f5e5f1a4235226ab
SeniorIntern <tnikhil.official@gmail.com>
2024-01-01 20:25:31 +0545

    Use persist middleware for preserve zustand store

--- app/store.ts
+++ app/store.ts
@@ -1,11 +1,12 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type UserStore = {
   token: string;
   setToken: (newToken: string) => void;
 };
-
 /*
+
 const useUserStore = create<UserStore>((set) => ({
   token: localStorage.getItem('token') || '', // Initialize with the token from localStorage
   setToken: (newToken: string) => {
@@ -14,11 +15,15 @@ const useUserStore = create<UserStore>((set) => ({
   }
 }));
 */
-const useUserStore = create<UserStore>((set) => ({
-  token: '', // Initialize with the token from localStorage
-  setToken: (newToken: string) => {
-    set({ token: newToken });
-  }
-}));
+
+const useUserStore = create(persist<UserStore>((set) => ({
+  token: '',
+  setToken: (newToken: string) => set({ token: newToken }),
+}),
+  {
+    name: 'token', // name of the item in the storage (must be unique)
+  },
+))
+
 
 export default useUserStore;
