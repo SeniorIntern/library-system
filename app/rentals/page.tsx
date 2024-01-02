e244b3113da628a47afe2f7140a64cddfd7db9f0
SeniorIntern <tnikhil.official@gmail.com>
2024-01-02 10:49:58 +0545

    Fix PATCH request fail

--- app/rentals/page.tsx
+++ app/rentals/page.tsx
@@ -3,30 +3,32 @@
 import { Button, Flex, Table, Text } from "@radix-ui/themes";
 import useRentals from "../hooks/useRentals";
 import { apiClient } from "../services/api-client";
-import { useState } from "react";
 
 export default function page() {
   const { rentals, setRentals, error, isLoading } = useRentals()
-  const [deleteError, setDeleteError] = useState<string>()
-
-  if (isLoading) return <Text>loading...</Text>
-  if (error) return <Text>{error}</Text>
 
   const handleReturn = async (rentalId: string) => {
-    const oldRentals = [...rentals || []]
-    setRentals(rentals?.filter(rental => rental._id !== rentalId))
+    const originalRentals = [...rentals || []]
+    setRentals(rentals?.map(r => r._id === rentalId ? { ...r, hasReturned: true } : r))
 
-    apiClient.delete('/rentals/' + rentalId).catch(err => {
-      setDeleteError(err.message)
-      setRentals(oldRentals)
-    })
+    apiClient.post('/returns', { rentalId })
+      .then(res => {
+        console.log(res.data)
+        apiClient.patch('/rentals/' + rentalId)
+          .catch(err => console.log(err.message))
+      }).catch(err => {
+        console.log(err.messgae)
+        setRentals(originalRentals)
+      })
 
-    // POST on returns
   }
+
+  if (isLoading) return <Text>loading...</Text>
+  if (error) return <Text color="red">{error}</Text>
+  if (!rentals?.length) return <Text>No Rentals</Text>
 
   return (
     <>
-      {deleteError && <Text color="red">{deleteError}</Text>}
       <Flex direction='row' justify="center" className="h-[70vh] items-center">
         <Table.Root variant="surface" className="w-1/2">
           <Table.Header>
@@ -34,7 +36,7 @@ export default function page() {
               <Table.ColumnHeaderCell>Book Name</Table.ColumnHeaderCell>
               <Table.ColumnHeaderCell>Customer Name</Table.ColumnHeaderCell>
               <Table.ColumnHeaderCell>Rent Date</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>Option</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Return Status/Options</Table.ColumnHeaderCell>
             </Table.Row>
           </Table.Header>
 
@@ -44,7 +46,12 @@ export default function page() {
                 <Table.RowHeaderCell>{rental.book.title}</Table.RowHeaderCell>
                 <Table.Cell>{rental.customer.name}</Table.Cell>
                 <Table.Cell>{new Date(rental.dateOut).toLocaleString()}</Table.Cell>
-                <Table.Cell><Button onClick={() => handleReturn(rental._id)}>Set Return</Button></Table.Cell>
+                {
+                  rental.hasReturned ?
+                    <Table.Cell>Returned</Table.Cell>
+                    :
+                    <Table.Cell><Button onClick={() => handleReturn(rental._id)}>Mark Return</Button></Table.Cell>
+                }
               </Table.Row>
             ))}
           </Table.Body>
