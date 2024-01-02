17282bca046c7a671a24bff64de5eaf76418d567
SeniorIntern <tnikhil.official@gmail.com>
2024-01-02 10:53:31 +0545

    Fix hydration issue

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 
 import { Text } from '@radix-ui/themes'
-import React from 'react'
+import React, { useEffect } from 'react'
 import useUserStore from '../store'
 import { redirect } from 'next/navigation'
 import Link from 'next/link'
 
 function page() {
   const { token } = useUserStore()
-  if (!token) return redirect('/login')
+  useEffect(() => {
+    if (!token) return redirect('/')
+  }, [token])
 
   const bookLinks = [
     { id: 1, label: "New Book", href: "/books/new" },
@@ -22,8 +24,8 @@ function page() {
       <section className="grid grid-cols-2 gap-6 p-4 md:p-6">
         {bookLinks.map(bl =>
         (
-          <Link href={bl.href}>
-            <div key={bl.id} className="flex items-center justify-center bg-gray-200 rounded-lg shadow-md h-32 w-44">
+          <Link href={bl.href} key={bl.id}>
+            <div className="flex items-center justify-center bg-gray-200 rounded-lg shadow-md h-32 w-44">
               <Text className="text-lg font-bold text-center">{bl.label}</Text>
             </div>
           </Link>
