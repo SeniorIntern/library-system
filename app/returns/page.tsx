c8b2661305c0a4e7d46405d7343f552de11210da
SeniorIntern <tnikhil.official@gmail.com>
2024-01-02 10:50:41 +0545

    Build the returns page

--- app/returns/page.tsx
+++ app/returns/page.tsx
@@ -1,5 +1,39 @@
+'use client'
+
+import { Flex, Table, Text } from "@radix-ui/themes"
+import useReturns from "../hooks/useReturns"
+
 export default function page() {
+  const { returns, error, isLoading } = useReturns()
+  console.log(returns);
+
+  if (isLoading) return <Text>loading...</Text>
+  if (error) return <Text>{error}</Text>
+  if (!returns?.length) return <Text>No Returns</Text>
+
   return (
-    <div>Returns page</div>
+    <>
+      <Flex direction='row' justify="center" className="h-[70vh] items-center">
+        <Table.Root variant="surface" className="w-1/2">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Book Name</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Customer Name</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>Return Date</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {returns?.map(r => (
+              <Table.Row key={r._id}>
+                <Table.RowHeaderCell>{r.rental.book.title}</Table.RowHeaderCell>
+                <Table.Cell>{r.rental.customer.name}</Table.Cell>
+                <Table.Cell>{new Date(r.dateReturned).toLocaleString()}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </Flex>
+    </>
   )
 }
