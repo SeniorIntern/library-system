dd3ae935d93ef5c677bc23e44f63d482987d5e5c
SeniorIntern <tnikhil.official@gmail.com>
2024-01-06 18:50:35 +0545

    Set up toast notification

--- app/books/rent/page.tsx
+++ app/books/rent/page.tsx
@@ -9,6 +9,7 @@ import { CSSProperties, useState } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
+import toast, { Toaster } from 'react-hot-toast'
 
 export default function page() {
   const inputStyle: CSSProperties = {
@@ -35,71 +36,79 @@ export default function page() {
 
   const schema = z.object({
     username: z.string().min(3, { message: "Name is required" }),
-    email: z.string().min(3, { message: "Email is required" }),
+    email: z.string().email().min(3, { message: "Email is required" }),
     address: z.string().min(3, { message: "Address is required" }),
   })
 
   const { handleSubmit, register, formState: { errors } } = useForm<z.infer<typeof schema>>({ resolver: zodResolver(schema) })
   const onSubmit = (data: FieldValues) => {
     console.log(data);
+    const { username, email, address } = data
+    apiClient.post('/rentals', { username, email, address, bookId: book?._id })
+      .then(() => {
+        toast.success('Book rented sucessfully!')
+      }).catch((err) => toast.error(err?.message))
   }
 
   return (
-    <Flex direction='column' width='100%' align='center' justify="center">
-      <select
-        aria-placeholder="Book title"
-        style={inputStyle}
-        className='mb-4'
-        onChange={(e) => {
-          findBookById(e.target.value)
-        }}
-      >
-        <option value="">Select a Book</option>
-        {books.map(book => (
-          <option
-            key={book._id}
-            value={book._id}
-          >
-            {book.title}
-          </option>
-        ))}
-      </select>
-
-      {book &&
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col gap-4 border-[1px] border-black rounded-md items-center w-[40vw] py-8"
+    <>
+      <Toaster />
+      <Flex direction='column' width='100%' align='center' justify="center">
+        <select
+          aria-placeholder="Book title"
+          style={inputStyle}
+          className='mb-4'
+          onChange={(e) => {
+            findBookById(e.target.value)
+          }}
         >
-          <Grid>
-            <input
-              {...register('username')}
-              id="username"
-              placeholder="Name of the User"
-              style={inputStyle}
-            />
-            {errors.username && <span style={errorStyle}>{errors.username.message}</span>}
-          </Grid>
-          <Grid>
-            <input
-              {...register('email')}
-              id="email"
-              placeholder="Email"
-              style={inputStyle}
-            />
-            {errors.email && <span style={errorStyle}>{errors.email.message}</span>}
-          </Grid>
-          <Grid>
-            <input
-              {...register('address')}
-              id="address"
-              placeholder="address"
-              style={inputStyle}
-            />
-            {errors.address && <span style={errorStyle}>{errors.address.message}</span>}
-          </Grid>
-          <Button type='submit'>RENT</Button>
-        </form>
-      }
-    </Flex>
+          <option value="">Select a Book</option>
+          {books.map(book => (
+            <option
+              key={book._id}
+              value={book._id}
+            >
+              {book.title}
+            </option>
+          ))}
+        </select>
+
+        {book &&
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-4 border-[1px] border-black rounded-md items-center w-[40vw] py-8"
+          >
+            <Grid>
+              <input
+                {...register('username')}
+                id="username"
+                placeholder="Name of the User"
+                style={inputStyle}
+              />
+              {errors.username && <span style={errorStyle}>{errors.username.message}</span>}
+            </Grid>
+            <Grid>
+              <input
+                {...register('email')}
+                id="email"
+                placeholder="Email"
+                style={inputStyle}
+              />
+              {errors.email && <span style={errorStyle}>{errors.email.message}</span>}
+            </Grid>
+            <Grid>
+              <input
+                {...register('address')}
+                id="address"
+                placeholder="address"
+                style={inputStyle}
+              />
+              {errors.address && <span style={errorStyle}>{errors.address.message}</span>}
+            </Grid>
+            <Button type='submit'>RENT</Button>
+          </form>
+        }
+      </Flex>
+    </>
   )
 }
